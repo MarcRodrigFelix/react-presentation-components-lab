d92aebad01862ca1615b4e4061c610a7a4583b39
Marcos <m.rodrig.felix@gmail.com>
2021-04-22 19:53:11 -0700

    commit tree e0a87f7a3f93c0e9a403c707613f6216ee6ada24

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={ () => { console.log('this prop works') } } />
   </div>,
   document.getElementById('root')
 );
